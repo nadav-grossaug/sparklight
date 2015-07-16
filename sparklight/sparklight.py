32829b01777b0ae541cad64609dbcf944d5b3db3
ami-navon <ami.navon@gmail.com>
2015-07-16 11:14:09 +0300

    added initial version

---
+++ sparklight/sparklight.py
@@ -0,0 +1,255 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+from __future__ import print_function
+import sys
+import argparse
+import os
+import gzip
+import glob
+from operator import add 
+
+
+SPARKLIGHT_VERBOSE=False
+class SparklightContext:
+    def __init__(self):
+        pass
+    def textFile(self, paths):
+        rdd = SparklightRdd()
+        if "," in paths: 
+            rdd.paths=paths.split(",")
+        else:
+            rdd.paths=paths
+        return rdd
+    @staticmethod
+    def each_line(inputs, limit=-1):
+        if inputs!=None and isinstance(inputs,list):
+            count = 0
+            for input in inputs:
+                for line in SparklightContext.each_line(input):
+                    yield line
+                    count+=1
+                    if count==limit:
+                        return
+            return
+    
+        filename = inputs
+        if filename==None or filename=="-" or filename=="stdin":
+            stream = sys.stdin
+        elif "*" in filename:
+            for line in SparklightContext.each_line(glob.glob(filename), limit):
+                yield line
+            return
+        elif filename.endswith(".gz"):
+            if SPARKLIGHT_VERBOSE:
+                sys.stderr.write("RDD Open gz file for reading: "+filename+"\n")
+            stream = gzip.open(filename,'rb')
+        else:
+            if SPARKLIGHT_VERBOSE:
+                sys.stderr.write("RDD Open file for reading: "+filename+"\n")
+            stream = open(filename, "rb")
+
+        count = 0
+        for line in stream:
+            try:
+                line = unicode(line, 'utf8').strip()
+                yield line
+                count+=1
+                if count==limit:
+                    break
+            except UnicodeDecodeError, ex:
+                if SPARKLIGHT_VERBOSE:
+                    sys.stderr.write("Exception UnicodeDecodeError "+str(ex)+" for line: "+line+"\n")
+                
+        stream.close()
+
+class SparklightRdd:
+    def __init__(self, parent_rdd=None):
+        self.data=None
+        self.paths=None
+        self.mapper=None
+        self.flat_mapper=None
+        self.parent_rdd=parent_rdd
+        self.group_by_key=None
+        self.group_by_value=None
+        self.filter_func=None
+        self.reduce_by_key=None
+        self.flag_distinct=False
+        self.sort_by=None
+        self.sort_by_ascending=True
+    def repartition(self, partitions):
+        return self
+    def cache(self):
+        self.data=self.collect()
+        return self
+    def collect(self):
+        return self.take(-1)
+    def first(self):
+        out = self.take(1)
+        if len(out)==0:
+            return None
+        return out[0]
+    def take(self, top_k):
+        return [x for x in self.yield_rdd(top_k)]
+    def count(self):
+        if self.data:
+            return len(self.data)
+        count = 0
+        for x in self.yield_rdd():
+            count+=1
+        return count
+    def yield_rdd(self, top_k=-1):
+        # sort_by :
+        if self.sort_by:
+            lines=[line for line in self.yield_raw()]
+            sorted_lines = sorted(lines, 
+                key=self.sort_by, reverse=self.sort_by_ascending)
+            count=0
+            for line in sorted_lines:
+                yield line
+                count+=1
+                if count==top_k:
+                    return
+            return
+        # ### distinct
+        if self.flag_distinct:
+            hash={}
+            for line in self.yield_raw():
+                if line not in hash:
+                    hash[line]=0
+                hash[line]+=1
+            count=0
+            for key, count in hash.iteritems():
+                yield key
+                count+=1
+                if count==top_k:
+                    return
+            return
+        # ### reduce_by_key:
+        if self.reduce_by_key:
+            hash={}
+            for line in self.yield_raw():
+                key=line[0]
+                value=line[1]
+                if key not in hash:
+                    hash[key]=value
+                else:
+                    hash[key]=self.reduce_by_key(hash[key],value)
+            count=0
+            for key, value in hash.iteritems():
+                yield (key,value)
+                count+=1
+                if count==top_k:
+                    return
+            return
+        # ### group-by
+        if self.group_by_key:
+            hash={}
+            for line in self.yield_raw():
+                key=self.group_by_key(line)
+                if key not in hash:
+                    hash[key]=[]
+                hash[key].append(self.group_by_value(line))
+            count=0
+            for key, arr in hash.iteritems():
+                yield (key,arr)
+                count+=1
+                if count==top_k:
+                    return
+            return
+        elif self.mapper:
+            # ### map
+            for line in self.yield_raw(top_k):
+                yield self.mapper(line)
+            return
+            # ### flat-map
+        elif self.flat_mapper:
+            count=0
+            for line in self.yield_raw():
+                # print(line) #REMREM
+                for o in self.flat_mapper(line):
+                    count+=1
+                    yield o
+                    if count==top_k:
+                        return
+            return
+        # ### filter
+        elif self.filter_func:
+            count=0
+            for line in self.yield_raw():
+                if self.filter_func(line):
+                    count+=1
+                    yield line
+                    if count==top_k:
+                        return
+            return
+        else:
+            for line in self.yield_raw(top_k):
+                yield line
+                
+    def yield_raw(self, top_k=-1):
+        if self.data is not None:
+            for line in self.data[0:top_k]:
+                yield line
+        elif self.paths is not None:
+            for line in SparklightContext.each_line(self.paths, top_k):
+                yield line
+        elif self.parent_rdd is not None:
+            for line in self.parent_rdd.yield_rdd(top_k):
+                yield line
+
+    def filter(self, func):
+        rdd = SparklightRdd(self)
+        rdd.filter_func=func
+        return rdd
+    def map(self, func):
+        rdd = SparklightRdd(self)
+        rdd.mapper=func
+        return rdd
+    def flatMap(self, func):
+        rdd = SparklightRdd(self)
+        rdd.flat_mapper=func
+        return rdd
+    def groupByKey(self):
+        return self.groupBy(None)
+    def groupBy(self, func):
+        rdd = SparklightRdd(self)
+        if func==None: # groupByKey:
+            rdd.group_by_key=lambda x:x[0]
+            rdd.group_by_value=lambda x:x[1]
+        else:
+            rdd.group_by_key=func
+            rdd.group_by_value=lambda x:x 
+        return rdd
+    def reduceByKey(self, func):
+        rdd = SparklightRdd(self)
+        rdd.reduce_by_key=func
+        return rdd
+    def distinct(self):
+        rdd = SparklightRdd(self)
+        rdd.flag_distinct=True
+        return rdd
+    def sortBy(self, keyfunc, ascending=True, numPartitions=None):
+        if keyfunc==None:
+            keyfunc=lambda x:x[0]
+        rdd = SparklightRdd(self)
+        rdd.sort_by=keyfunc
+        rdd.sort_by_ascending=(ascending==True)
+        return rdd
+    def sortByKey(self, ascending=True, numPartitions=None, keyfunc=None):
+        return self.sortBy(keyfunc,ascending,numPartitions)
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i","--input", type=str, dest='input', nargs='+', default=None,help="Input files")
+    start_options = parser.parse_args()
+    
+    
+    sc=SparklightContext()
+    for x in sc.textFile(start_options.input) \
+            .repartition(100) \
+            .map(lambda line: line.split("\t")) \
+            .take(4):
+        print(x)
+    
---
+++ sparklight/timevector.py
@@ -0,0 +1,132 @@
+#! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+from __future__ import print_function
+import sys
+import argparse
+import os
+from operator import add 
+from collections import defaultdict
+import arrow
+import time
+import numpy as np
+
+EPOCH=1970
+DELTA_WINDOW=60
+
+
+class TimeVector:
+    def __init__(self, start_date, end_date, start_hour="00:00:00", end_hour="23:59:00", timewindow_mins=DELTA_WINDOW):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.start_hour = start_hour
+        self.end_hour = end_hour
+        self.delta = timewindow_mins
+        self.deltas_per_day = 24.0*60/self.delta
+        self.start_index = self.to_timeindex(self.start_date, self.start_hour, False)
+        self.len=self.to_timeindex(self.end_date, self.end_hour)+1
+        self.vec = np.zeros(self.len)
+    def vector(self):
+        return self.vec
+    def inc(self, date, hour, increment=1):
+        if increment!=0:
+            index = self.to_timeindex(date, hour)
+            self.vec[index]+=increment
+    def to_timeindex(self, date, hour, relative_to_start_time=True):
+        hour = hour.split(".")[0] # omit_milliseconds
+        #     timestamp = time.strptime(hour,"%H:%M:%S") # <= '23:58:21.574703'
+        timestamp = time.strptime(date+" "+hour,"%Y-%m-%d %H:%M:%S")
+        hour_index = (timestamp.tm_hour*60.0+timestamp.tm_min)/self.delta
+        date_index = ((timestamp.tm_year-EPOCH)*365+timestamp.tm_yday) * self.deltas_per_day
+        index= int(date_index + hour_index)
+        if relative_to_start_time:
+            index -= self.start_index
+        return index
+    def from_tuples(self, tuples, value_func=None):
+        for date, hour, value in tuples:
+            if value_func:
+                value = value_func(value)
+            self.inc(date, hour, value)
+        return self.vec
+    @staticmethod 
+    def to_extent(rdd, date_func):
+        min_date = rdd.map(date_func).min()
+        max_date = rdd.map(date_func).max()
+        return (min_date,max_date)
+    @staticmethod 
+    def to_v(tuples, value_func=None, start_date=None, end_date=None,timewindow_mins=DELTA_WINDOW):
+        if start_date==None:
+            start_date=min([date for date,hour,value in tuples])
+        if end_date==None:
+            end_date=max([date for date,hour,value in tuples])
+        v = TimeVector( start_date, end_date, timewindow_mins=timewindow_mins )
+        return v.from_tuples(tuples, value_func)
+
+def timevector_to_v(tuples, value_func=None, start_date=None, end_date=None,timewindow_mins=DELTA_WINDOW):
+    if start_date==None:
+        start_date=min([date for date,hour,value in tuples])
+    if end_date==None:
+        end_date=max([date for date,hour,value in tuples])
+    v = TimeVector( start_date, end_date, timewindow_mins=timewindow_mins )
+    return v.from_tuples(tuples, value_func)
+    
+    
+    
+class HashTimeVector:
+    def __init__(self, start_date=None, end_date=None, start_hour="00:00:00", end_hour="23:59:00", timewindow_mins=DELTA_WINDOW):
+        self.start_date = start_date
+        self.end_date = end_date
+        self.start_hour = start_hour
+        self.end_hour = end_hour
+        self.delta = timewindow_mins
+        self.deltas_per_day = 24.0*60/self.delta
+        self.hash = defaultdict(int)
+    def vector(self):
+        if self.start_date is None and self.end_date is None:
+            self.start_date, self.end_date = self.to_extent()
+        tv = TimeVector(self.start_date, self.end_date, timewindow_mins=self.delta)
+        tv.from_tuples([ (x[0][0], x[0][1], x[1]) for x in self.hash.iteritems()  ])
+        return tv.vector()
+    def to_axis(self, as_int=False):
+        vec=self.vector()
+        axis=[]
+        min_date, ma_date = self.to_extent()
+        date = arrow.get(min_date, 'YYYY-MM-DD')
+        for i in xrange(vec.shape[0]):
+            if as_int:
+                axis.append(date.timestamp)
+            else:
+                axis.append(date.format('YYYY-MM-DD HH:mm:ss'))
+            date = date.replace(minutes=self.delta)
+        return axis,vec
+    def to_extent(self):
+        min_date = min([x[0] for x in self.hash.iterkeys()])
+        max_date = max([x[0] for x in self.hash.iterkeys()])
+        return (min_date,max_date)
+    def force_extent(self,start_date, end_date):
+        self.start_date = start_date
+        self.end_date = end_date
+    def inc(self, date, hour, increment=1):
+        if increment!=0:
+            ts = self.to_timequantization(date, hour)
+            self.hash[ts]+=increment
+    def to_timequantization(self, date, hour):
+        hour = hour.split(".")[0] # omit_milliseconds
+        ts = arrow.get("%s %s" % (date, hour), 'YYYY-MM-DD HH:mm:ss')
+        ts_quantization = ts.replace(second=0, minutes= -1*((ts.hour*60+ts.minute) % self.delta))
+        return (ts_quantization.format('YYYY-MM-DD'),ts_quantization.format('HH:mm:ss'))
+
+SPARKLINES_CHARS= u"▁▂▃▄▅▆▇█"
+
+def numpy_to_sparkline_string(vec):
+        max_value=vec.max()
+        min_value=vec.min()
+        if max_value==min_value:
+            return SPARKLINES_CHARS[0]*len(vec)
+        sparkline=[SPARKLINES_CHARS[
+                int(7.0*(i-min_value)/(max_value-min_value))
+            ] for i in x[1]
+        ]
+        return "".join(sparkline)
+
+if __name__ == "__main__":
+    pass
